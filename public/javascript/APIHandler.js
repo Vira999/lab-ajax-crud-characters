f2ab0ece00a73b73f2b18d080ec0647193392b6b
Vira Sholinian <105912497+Vira999@users.noreply.github.com>
2022-11-25 20:52:19 +0100

    Add files via upload

--- public/javascript/APIHandler.js
+++ public/javascript/APIHandler.js
@@ -4,22 +4,32 @@ class APIHandler {
   }
 
   getFullList () {
+    axios.get(this.BASE_URL)
+      .then(dataRetrieved => console.log(dataRetrieved))
+      .catch(err => console.log("The error of all characters", err));
+  };
 
+  getOneRegister (id) {
+    axios.get(`${this.BASE_URL}/${id}`)
+    .then(dataRetrieved => console.log(dataRetrieved))
+    .catch(err => console.log("The error of one character", err));
   }
 
-  getOneRegister () {
-
+  createOneRegister (characterInfo) {
+    axios.post(`${this.BASE_URL}`, characterInfo)
+    .then(dataPosted => console.log(dataPosted))
+    .catch(err => console.log("The error creating one character", err));
   }
 
-  createOneRegister () {
-
+  updateOneRegister (id, updatedCharacterInfo) {
+    axios.patch(`${this.BASE_URL}/${id}`, updatedCharacterInfo)
+    .then(dataPatched => console.log(dataPatched))
+    .catch(err => console.log("The error of update one character", err));
   }
 
-  updateOneRegister () {
-
-  }
-
-  deleteOneRegister () {
-
+  deleteOneRegister (id) {
+    axios.delete(`${this.BASE_URL}/${id}`)
+    .then(dataDelete => console.log("The character deleted successfully!"))
+    .catch(err => console.log("The error delete one character", err));
   }
 }
